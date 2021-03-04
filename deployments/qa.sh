22f07bae710cd465aa51d3297fbf3b9618e40f42
Domingo Blanco <domingoa.blanco@gmail.com>
2021-03-04 17:19:00 -0500

    cambio en el sh

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
 sudo npm install -g --unsafe-perm now
 echo "deploying..."
-URL=$(now --docker --public -t $NOW_CONFIG)
+URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
 curl --silent -L $URL 
